b6d7839d26a0495ac74faab3c0bff2b93a9aed81
LeonardoGarciah <leonardoformulaum@hotmail.com>
2023-01-12 21:55:10 -0300

    feat: created categories component

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import {ScrollView} from "react-native";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
-    return (
 
+    const categories = [
+        {
+            title: "Lanches",
+            imgUrl: "https://links.papareact.com/wru",
+        },
+        {
+            title: "Bebidas",
+            imgUrl: "https://links.papareact.com/wru",
+        },
+    ]
+
+    const renderCategories = () => {
+        return categories.map((category) => {
+            const { imgUrl, title } = category;
+
+            return <CategoryCard imgUrl={imgUrl} title={title} />
+        })
+    }
+
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                paddingHorizontal: 14,
+                paddingTop: 10
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+        >
+            {renderCategories()}
+        </ScrollView>
     )
 }
 
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,7 +4,7 @@ import {FadersHorizontal, MagnifyingGlass} from "phosphor-react-native";
 
 const Search = () => {
     return (
-        <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'>
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
             <View className='flex-row flex-1 space-x-2 bg-gray-200 p-2'>
                 <MagnifyingGlass color='gray' size={20} />
                 <TextInput
