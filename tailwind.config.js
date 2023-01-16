0ea7df67129139a9031703d03505bb0fa2707b1f
LeonardoGarciah <leonardoformulaum@hotmail.com>
2023-01-16 20:47:33 -0300

    fix: fixed tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primary: '#00CCBB',
-    }
+    extend: {
+      colors: {
+        primary: '#00CCBB',
+      }
+    },
   },
   plugins: [],
 }
